8a607f13fc19ae5eafbdcdaa8226e9305b5f3922
Caio Torres Seares <caiot.seares@gmail.com>
2025-07-28 01:33:45 -0300

    refactor: remove headless

--- src/extraction/kabum_scraper.py
+++ src/extraction/kabum_scraper.py
@@ -41,7 +41,7 @@ def kabum_scraper(base_url: str, product_type: str):
     
     try:
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)  # set to False to see the browser
+            browser = p.chromium.launch(headless=True)  # set to False to see the browser
             page = browser.new_page(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36")
             
             while True:
